2ae262ebc39f1ce9152ad77ed91ae767a79e4744
Carren284 <carrenkoskei0@gmail.com>
2024-10-12 13:25:50 +0300

    Add files via upload

---
+++ Do while loop controll.c
@@ -0,0 +1,29 @@
+//do while loop control
+/*Name:Carren chaerotich
+Regno:CT101/G/22817/2024
+*/
+#include<stdio.h>
+int main()
+{
+  int start,stop,sum=0;
+  printf("Enter start:");
+  scanf("%d",&start);
+  printf("Enter when to stop:");
+  scanf("%d",&stop);
+  do {printf("%d\n",start);
+  
+  sum=sum+start;
+  sum+=start;
+  start++;
+  }
+  while(start,start<=stop);
+  printf("The sum%d",sum);
+  return 0;
+  
+  }
+
+
+// Language:C 
+// ProjectType:0 
+// Copy the full code and open the CCoder APP to run it. 
+// CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
